fce4534568bdad79b8af5f96c2deb1e7e6afa628
Sam Gaus <sam@gaus.co.uk>
2025-06-04 13:09:11 +0100

    Do not stretch images

--- src/components/EffectList.tsx
+++ src/components/EffectList.tsx
@@ -19,7 +19,7 @@ export function EffectList({ effects }: Props) {
     <ul className={listStyle}>
       {effects.map((effect, index) => (
         <li key={index}>
-          <Stack direction="horizontal" title={`Effect id ${effect.id}`}>
+          <Stack direction="horizontal" title={`Effect id ${effect.id}`} alignItems="center">
             <img
               src={`https://d2uyhvukfffg5a.cloudfront.net/itemimages/${effect.image}`}
               alt={effect.name}
