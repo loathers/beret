627e524f2849ed6ae1b11b6051c69b6b5b77d997
Sam Gaus <sam@gaus.co.uk>
2025-06-04 13:43:05 +0100

    Clickable wiki links

--- src/components/EffectList.tsx
+++ src/components/EffectList.tsx
@@ -1,8 +1,6 @@
 import { css } from "../../styled-system/css";
-import { Stack } from "../../styled-system/jsx";
 import type { Effect } from "../data";
-import { Modifiers } from "./Modifiers";
-import { Text } from "./Text";
+import { EffectListItem } from "./EffectListItem";
 
 type Props = {
   effects: Effect[];
@@ -18,28 +16,7 @@ export function EffectList({ effects }: Props) {
   return (
     <ul className={listStyle}>
       {effects.map((effect, index) => (
-        <li key={index}>
-          <Stack
-            direction="horizontal"
-            title={`Effect id ${effect.id}`}
-            alignItems="center"
-          >
-            <img
-              src={`https://d2uyhvukfffg5a.cloudfront.net/itemimages/${effect.image}`}
-              alt={effect.name}
-            />
-            <Stack gap={1}>
-              <Text>
-                You acquire an effect: <b>{effect.name}</b> (10 Adventures)
-              </Text>
-              {effect.effectModifierByEffect && (
-                <Modifiers
-                  modifiers={effect.effectModifierByEffect.modifiers}
-                />
-              )}
-            </Stack>
-          </Stack>
-        </li>
+        <EffectListItem effect={effect} key={index} />
       ))}
     </ul>
   );
---
+++ src/components/EffectListItem.tsx
@@ -0,0 +1,41 @@
+import { Stack } from "../../styled-system/jsx";
+import type { Effect } from "../data";
+import { Text } from "./Text";
+import { Modifiers } from "./Modifiers";
+
+type Props = {
+  effect: Effect;
+};
+
+export function EffectListItem({ effect }: Props) {
+  const modifiers = effect.effectModifierByEffect?.modifiers;
+  const url = `https://kol.coldfront.net/thekolwiki/index.php/${modifiers?.["Wiki Name"]?.slice(1, -1) ?? effect.name}`;
+  return (
+    <li>
+      <Stack
+        direction="horizontal"
+        title={`Effect id ${effect.id}`}
+        alignItems="center"
+      >
+        <a href={url} target="_blank">
+          <img
+            src={`https://d2uyhvukfffg5a.cloudfront.net/itemimages/${effect.image}`}
+            alt={effect.name}
+          />
+        </a>
+        <Stack gap={1}>
+          <Text>
+            You acquire an effect:{" "}
+            <b>
+              <a href={url} target="_blank">
+                {effect.name}
+              </a>
+            </b>{" "}
+            (10 Adventures)
+          </Text>
+          {modifiers && <Modifiers modifiers={modifiers} />}
+        </Stack>
+      </Stack>
+    </li>
+  );
+}
